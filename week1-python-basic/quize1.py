ac02618af7394f61c5c545cd85262204fd5b5ab5
Etienne Wang <yitianwang1991@gmail.com>
2023-06-02 20:31:51 -0700

    modified quiz1 and added week2 folder

--- week1-python-basic/quize1.py
+++ week1-python-basic/quize1.py
@@ -1,4 +1,5 @@
 def showReadingGlassesOrder (firstName, lastName, address, country, lenses, blueLightProction):
+    print('********************')
     if country == "Canada":
         shippingFee = '$25.00'
     elif country == "US":
@@ -7,9 +8,9 @@ def showReadingGlassesOrder (firstName, lastName, address, country, lenses, blue
         shippingFee = '$26.50'
     
     if blueLightProction:
-        print(f"{firstName} {lastName}\n{address}\n -----------------\n Lenses: {lenses}x (Blue light protection included)\n shipping charges are {shippingFee}.")
+        print(f"{firstName} {lastName}\n{address}\n -----------------\nLenses: {lenses}x (Blue light protection included)\nshipping charges are {shippingFee}.")
     else:
-        print(f"{firstName} {lastName}\n{address}\n -----------------\n Lenses: {lenses}x\n shipping charges are {shippingFee}.")
+        print(f"{firstName} {lastName}\n{address}\n -----------------\nLenses: {lenses}x\nshipping charges are {shippingFee}.")
 
 
 showReadingGlassesOrder("Akshay", "Tandon", "12 Peaceful Place", "Canada", 1.25, False)
